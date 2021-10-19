bb644d0747d92df13f579b4c7d07df4aeb216116
GMNaim <gmnaim3336@gmail.com>
2021-10-19 13:53:33 +0600

    Added logger to see user in model_signals.py file.

--- easyaudit/signals/model_signals.py
+++ easyaudit/signals/model_signals.py
@@ -81,10 +81,12 @@ def pre_save(sender, instance, raw, using, update_fields, **kwargs):
             try:
                 user = get_current_user()
                 # validate that the user still exists
-                logger.info(f"USER: {user}")
+                logger.info(f"get_current_user:{get_current_user()}")
+                logger.info(f"USER: {user}, {get_current_user()}")
                 if user:
                     user = get_user_model().objects.get(auth_id=user.pk)
             except:
+                logger.info(f"get_current_user in except:{get_current_user()}")
                 logger.info(f"USER: {user}")
                 if not user:
                     user = None
@@ -168,6 +170,7 @@ def post_save(sender, instance, created, raw, using, update_fields, **kwargs):
                 #     user = None
                 
                 # validate that the user still exists
+                logger.info(f"get_current_user:{get_current_user()}")
                 logger.info(f"USER: {user}")
                 if user:
                     user = get_user_model().objects.get(auth_id=user.pk)
@@ -291,6 +294,7 @@ def m2m_changed(sender, instance, action, reverse, model, pk_set, using, **kwarg
             try:
                 user = get_current_user()
                 # validate that the user still exists
+                logger.info(f"get_current_user:{get_current_user()}")
                 logger.info(f"USER: {user}")
                 if user:
                     user = get_user_model().objects.get(auth_id=user.pk)
@@ -356,6 +360,7 @@ def post_delete(sender, instance, using, **kwargs):
             try:
                 user = get_current_user()
                 # validate that the user still exists
+                logger.info(f"get_current_user:{get_current_user()}")
                 logger.info(f"USER: {user}")
                 if user:
                     user = get_user_model().objects.get(auth_id=user.pk)
